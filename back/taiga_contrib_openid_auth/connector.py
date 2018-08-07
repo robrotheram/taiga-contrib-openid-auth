ea1174ec6e3f4e33cf9c73b2a85416a7dd59feae
robrotheram <robrotheram@gmail.com>
2018-08-07 22:02:53 +0100

    Fixed versions

---
+++ back/taiga_contrib_openid_auth/connector.py
@@ -0,0 +1,148 @@
+# Copyright (C) 2014-2017 Andrey Antukh <niwi@niwi.nz>
+# Copyright (C) 2014-2017 Jesús Espino <jespinog@gmail.com>
+# Copyright (C) 2014-2017 David Barragán <bameda@dbarragan.com>
+# Copyright (C) 2014-2017 Alejandro Alonso <alejandro.alonso@kaleidos.net>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import requests
+import json
+
+from collections import namedtuple
+from urllib.parse import urljoin
+
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+from taiga.base.connectors.exceptions import ConnectorBaseException
+
+
+class OpenIDApiError(ConnectorBaseException):
+    pass
+
+
+######################################################
+## Data
+######################################################
+
+CLIENT_ID = getattr(settings, "OPENID_CLIENT_ID", None)
+CLIENT_SECRET = getattr(settings, "OPENID_CLIENT_SECRET", None)
+TOKEN_URL = getattr(settings, "OPENID_TOKEN_URL", None)
+USER_URL = getattr(settings, "OPENID_USER_URL", None)
+HEADERS = {"Accept": "application/json",}
+
+AuthInfo = namedtuple("AuthInfo", ["access_token"])
+User = namedtuple("User", ["id", "username", "full_name", "email"])
+
+######################################################
+## utils
+######################################################
+
+def _build_url(*args, **kwargs) -> str:
+    """
+    Return a valid url.
+    """
+    resource_url = API_RESOURCES_URLS
+    for key in args:
+        resource_url = resource_url[key]
+
+    if kwargs:
+        resource_url = resource_url.format(**kwargs)
+
+    return urljoin(API_URL, resource_url)
+
+
+def _get(url:str, headers:dict) -> dict:
+    """
+    Make a GET call.
+    """
+    response = requests.get(url, headers=headers)
+    print(response.json())
+
+    data = response.json()
+    if response.status_code != 200:
+        raise OpenIDApiError({"status_code": response.status_code,
+                                  "error": data.get("error", "")})
+    return data
+
+
+def _post(url:str, params:dict, headers:dict) -> dict:
+    """
+    Make a POST call.
+    """
+    print("Req", params)
+    response = requests.post(url, data=params, headers=headers)
+    print("RESPONSE", response.json())
+    data = response.json()
+    if response.status_code != 200 or "error" in data:
+        raise OpenIDApiError({"status_code": response.status_code,
+                                  "error": data.get("error", "")})
+    return data
+
+
+######################################################
+## Simple calls
+######################################################
+
+def login(access_code:str, redirect_uri:str, client_id:str=CLIENT_ID, client_secret:str=CLIENT_SECRET, headers:dict=HEADERS):
+          
+    """
+    Get access_token fron an user authorized code, the client id and the client secret key.
+    (See http://openid.net/specs/openid-connect-core-1_0.html#TokenEndpoint).
+    """
+    if not CLIENT_ID or not CLIENT_SECRET:
+        raise OpenIDApiError({"error_message": _("Login with openid-connect account is disabled. Contact "
+                                                     "with the sysadmins. Maybe they're snoozing in a "
+                                                     "secret hideout of the data center.")})
+
+    url = TOKEN_URL
+    params={"grant_type": "authorization_code",
+            "code": access_code,
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "redirect_uri": redirect_uri
+            }
+    data = _post(url, params=params, headers=headers)
+    return AuthInfo(access_token=data.get("access_token", None))
+
+
+def get_user_profile(headers:dict=HEADERS):
+    """
+    Get authenticated user info.
+    (See openid.net/specs/openid-connect-core-1_0.html#UserInfo).
+    """
+    
+    url = USER_URL
+    data = _get(url, headers=headers)
+    print (data)
+    return User(id=data.get("sub", None),
+                username=data.get("preferred_username", None),
+                full_name=data.get("name", None),
+                email=data.get("email", None),
+                
+            )       
+
+######################################################
+## Convined calls
+######################################################
+
+def me(access_code:str, redirect_uri:str) -> tuple:
+    """
+    Connect to a openid account and get all personal info (profile and the primary email).
+    """
+    auth_info = login(access_code, redirect_uri)
+    headers = HEADERS.copy()
+    headers["Authorization"] = "Bearer {}".format(auth_info.access_token)
+    user = get_user_profile(headers=headers)
+    return user
+
